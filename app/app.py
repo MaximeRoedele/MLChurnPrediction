99bf07e5477ba565bd0e2a3a2cb7a5124a1f6fa8
Maxime Roedelé <47213803+MaximeRoedele@users.noreply.github.com>
2024-03-29 16:06:30 +0000

    Minor changes and updated README

--- app/app.py
+++ app/app.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI, status, HTTPException
-from pydantic import BaseModel, Field
+# customer0 = Customer(customerID="BEEP", gender="Male", SeniorCitizen="No", Partner = "No", Dependents = "Yes", tenure = 5, PhoneService = "Yes",
+#                     MultipleLines = "No", InternetService = "DSL", OnlineSecurity = "No", OnlineBackup = "No", DeviceProtection = "No", TechSupport = "No",
+#                     StreamingTV = "No", StreamingMovies = "Yes", Contract = "One year", PaperlessBilling = "Yes", PaymentMethod = "Bank transfer (automatic)",
+#                     MonthlyCharges = 15.0, TotalCharges = 15.0*5)
+
+# customer1 = Customer(customerID="BEEP", gender="Male", SeniorCitizen="No", Partner = "No", Dependents = "No", tenure = 1, PhoneService = "Yes",
+#                     MultipleLines = "No", InternetService = "No", OnlineSecurity = "No internet service", OnlineBackup = "No internet service", DeviceProtection = "No internet service", TechSupport = "No internet service",
+#                     StreamingTV = "No internet service", StreamingMovies = "No internet service", Contract = "Month-to-month", PaperlessBilling = "No", PaymentMethod = "Mailed check",
+#                     MonthlyCharges = 20.15, TotalCharges = 20.15)
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 import torch
 import pandas as pd
@@ -8,6 +18,7 @@ from app_models import predict
 
 # Instanciate the FastAPI application
 app = FastAPI()
+
 
 # Define the pydantic dataclasses used by the application
 class Customer(BaseModel):
@@ -19,7 +30,7 @@ class Customer(BaseModel):
     tenure: int
     PhoneService: Literal["Yes", "No"]
     MultipleLines: Literal["Yes", "No", "No phone service"]
-    InternetService: Literal['No', 'DSL', 'Fiber optic']
+    InternetService: Literal["No", "DSL", "Fiber optic"]
     OnlineSecurity: Literal["Yes", "No", "No internet service"]
     OnlineBackup: Literal["Yes", "No", "No internet service"]
     DeviceProtection: Literal["Yes", "No", "No internet service"]
@@ -28,31 +39,26 @@ class Customer(BaseModel):
     StreamingMovies: Literal["Yes", "No", "No internet service"]
     Contract: Literal["Month-to-month", "One year", "Two year"]
     PaperlessBilling: Literal["Yes", "No"]
-    PaymentMethod: Literal["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"]
+    PaymentMethod: Literal[
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)",
+    ]
     MonthlyCharges: float
     TotalCharges: float
+
 
 class Responce(BaseModel):
     churn: Literal["Yes", "No"]
 
-customer0 = Customer(customerID="BEEP", gender="Male", SeniorCitizen="No", Partner = "No", Dependents = "Yes", tenure = 5, PhoneService = "Yes",
-                    MultipleLines = "No", InternetService = "DSL", OnlineSecurity = "No", OnlineBackup = "No", DeviceProtection = "No", TechSupport = "No",
-                    StreamingTV = "No", StreamingMovies = "Yes", Contract = "One year", PaperlessBilling = "Yes", PaymentMethod = "Bank transfer (automatic)",
-                    MonthlyCharges = 15.0, TotalCharges = 15.0*5)
-
-customer1 = Customer(customerID="BEEP", gender="Male", SeniorCitizen="No", Partner = "No", Dependents = "No", tenure = 1, PhoneService = "Yes",
-                    MultipleLines = "No", InternetService = "No", OnlineSecurity = "No internet service", OnlineBackup = "No internet service", DeviceProtection = "No internet service", TechSupport = "No internet service",
-                    StreamingTV = "No internet service", StreamingMovies = "No internet service", Contract = "Month-to-month", PaperlessBilling = "No", PaymentMethod = "Mailed check",
-                    MonthlyCharges = 20.15, TotalCharges = 20.15)
-
-
-#print(predict(customer.model_dump()))
 
 @app.get("/")
 def index():
     return {"Health check": "OK"}
 
-@app.post("/predict", response_model = Responce)
+
+@app.post("/predict", response_model=Responce)
 def predict_churn(payload: Customer):
     churn = predict(payload.model_dump())
     return {"churn": churn}
--- app/app_models.py
+++ app/app_models.py
@@ -1,7 +1,4 @@
 from ml_models import FCNN
-from pipelines.Preprocessing import ColumnDropper, NaNAmputator, FeatureEncoder, DataValidator 
-from fastapi.encoders import jsonable_encoder
-from sklearn.pipeline import Pipeline
 
 import _pickle
 from pathlib import Path
@@ -9,27 +6,29 @@ import torch
 import pandas as pd
 
 # Make sure to find the base directory, as this can vary in Docker containers
-BASE_DIR = Path("app_models.py").parent.resolve()
+BASE_DIR = Path("app_models.py").parent.resolve(strict=True)
 
 # Define paths to the desired model(s) and preprocessing pipeline
-PIPELINE_PATH = Path(f"{BASE_DIR}/pipelines/completed_pipelines/churn_preprocessing_V0.pkl")
 MODEL_PATH = Path(f"{BASE_DIR}/ml_models/trained_models/FCNN_churn_V0.pth")
+PIPELINE_PATH = Path(
+    f"{BASE_DIR}/pipelines/completed_pipelines/churn_preprocessing_V0.pkl"
+)
 
 # Load in the preprocessing pipeline using _pickle (C pickle)
 with open(PIPELINE_PATH, "rb") as f:
     pipeline = _pickle.load(f)
 
-
 # Load in the desired model(s) using pytorch's load
 model = torch.load(MODEL_PATH)
 
+
+# Define the prediction pipeline used by the API
 def predict(payload):
     data = pd.DataFrame.from_dict([payload], orient="columns")
     data = pipeline.transform(data)
     data = torch.from_numpy(data.values).type(torch.float32)
     churn = int(torch.round(torch.sigmoid(model(data))).item())
-    
+
     if churn == 1:
         return "Yes"
     return "No"
-
